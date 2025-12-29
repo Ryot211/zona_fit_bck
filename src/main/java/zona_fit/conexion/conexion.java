611015451d18b498c59422522c2afaabbb4dc5c3
Ryot211 <ryot211@gmail.com>
2025-12-28 23:01:51 -0500

    test1

--- src/main/java/zona_fit/conexion/conexion.java
+++ src/main/java/zona_fit/conexion/conexion.java
@@ -1,10 +1,32 @@
 package zona_fit.conexion;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class conexion {
 
     public static Connection getConexion(){
-        
+        Connection conexion = null;
+        var baseDatos = "zona_fit_db";
+        var url = "jdbc:mysql://localhost:3307/"+baseDatos;
+        var usuario = "zonafit_user";
+        var password = "Sumatra2025@";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url,usuario,password);
+        } catch (Exception e) {
+            System.out.println("Error al conectarnos a  la base de datos" +e.getMessage());
+        }
+        return conexion;
+    }
+
+    public static void main(String[] args) {
+        var conexion = getConexion();
+        if(conexion != null){
+            System.out.println("Conexion exitosa"+conexion);
+        }else{
+            System.out.println("Error al conectarnos a la base de datos ");
+        }
     }
 }
+
