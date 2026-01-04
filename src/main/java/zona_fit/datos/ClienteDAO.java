e8c0de45c014800d2d38047e4614002f7dfa28a9
Ryot211 <ryot211@gmail.com>
2026-01-03 21:24:16 -0500

    buscarCLiente

--- src/main/java/zona_fit/datos/ClienteDAO.java
+++ src/main/java/zona_fit/datos/ClienteDAO.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import static  zona_fit.conexion.Conexion.getConexion;
 
 public class ClienteDAO implements  IClienteDAO{
 
@@ -43,6 +44,29 @@ public class ClienteDAO implements  IClienteDAO{
 
     @Override
     public boolean buscarClientePorId(Cliente cliente) {
+        PreparedStatement ps;
+        ResultSet rs;
+        var con = getConexion();
+        var sql = "SELECT * FROM cliente WHERE id= ?";
+        try{
+            ps= con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            rs= ps.executeQuery();
+            if(rs.next()){// pregunta si tiene un registro a leer
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                return true;
+            }
+        }catch(Exception e){
+            System.out.println("Error al recuperar cliente por id : "+ e.getMessage());
+        }
+        finally {
+            try{
+                con.close();
+        }catch(Exception e){
+                System.out.println("Error al cerrar conexion "+e.getMessage());}
+        }
         return false;
     }
 
@@ -62,10 +86,19 @@ public class ClienteDAO implements  IClienteDAO{
     }
 
     public static void main(String[] args) {
-        // Listar Clientes
-        System.out.println("Listar Clientes");
         IClienteDAO clienteDAO = new ClienteDAO();
-        var clientes = clienteDAO.listarClientes();
-        clientes.forEach(System.out::println);
+        // Listar Clientes
+//        System.out.println("Listar Clientes");
+//        IClienteDAO clienteDAO = new ClienteDAO();
+//        var clientes = clienteDAO.listarClientes();
+//        clientes.forEach(System.out::println);
+        // buscar cliente por id
+        var cliente1 = new Cliente(1);
+        System.out.println("Cliuente antes de la busqueda "+cliente1);
+        var encontrado = clienteDAO.buscarClientePorId(cliente1);
+        if(encontrado)
+            System.out.println("Cliente encontrado "+cliente1);
+        else
+            System.out.println("Cliente no encontrado "+cliente1.getId());
     }
 }
