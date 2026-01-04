aefd7a6c65b73b0b4269b3400dbfdd79c834aaaa
Ryot211 <ryot211@gmail.com>
2026-01-03 22:34:21 -0500

    eliminar_cliente

--- src/main/java/zona_fit/datos/ClienteDAO.java
+++ src/main/java/zona_fit/datos/ClienteDAO.java
@@ -126,6 +126,23 @@ public class ClienteDAO implements  IClienteDAO{
 
     @Override
     public boolean eliminarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        var sql= "Delete from cliente where id= ?";
+        try{ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            ps.execute();
+            return  true;
+
+        }catch(Exception e){
+            System.out.println("Error al eliminar cliente: "+e.getMessage());
+        }finally{
+            try{
+                con.close();
+            }catch(Exception e){
+                System.out.println("Error al cerrar conexion "+e.getMessage());
+            }
+        }
         return false;
     }
 
@@ -145,13 +162,22 @@ public class ClienteDAO implements  IClienteDAO{
 //            System.out.println("Error al agregar cliente "+nuevoCliente);
 //        }
         // modificar cliente
-        var modificarCliente  = new Cliente(4,"Carmen","AS",300);
-        var modificado = clienteDAO.modificarCliente(modificarCliente);
-        if(modificado){
-            System.out.println("Cliente modificado");
+//        var modificarCliente  = new Cliente(4,"Carmen","AS",300);
+//        var modificado = clienteDAO.modificarCliente(modificarCliente);
+//        if(modificado){
+//            System.out.println("Cliente modificado");
+//        }else{
+//            System.out.println("Cliente modificado nulo");
+//        }
+        //* Eliminar Cliente
+        var EliminarCliente = new Cliente(4);
+        var eliminado = clienteDAO.eliminarCliente(EliminarCliente);
+        if(eliminado){
+            System.out.println("Cliente eliminado");
         }else{
-            System.out.println("Cliente modificado nulo");
+            System.out.println("Cliente no eliminado");
         }
+
 
         // Listar Clientes
         System.out.println("Listar Clientes");
